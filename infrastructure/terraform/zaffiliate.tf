df266d04c3703e7b4196cf177b8304c15c7bb904
cvsz <seaza@msn.com>
2026-08-24 21:39:12 +0000

    fix(infra): zaffiliate record as tunnel CNAME (A-record pointed at NAT-blocked origin; ingress entry pending dashboard add)

--- infrastructure/terraform/zaffiliate.tf
+++ infrastructure/terraform/zaffiliate.tf
@@ -1,18 +1,18 @@
-resource "cloudflare_record" "zaffiliate_a" {
+resource "cloudflare_record" "zaffiliate_cname" {
   zone_id = var.cloudflare_zone_id
   name    = "zaffiliate"
-  value   = var.zaffiliate_origin_ip
-  type    = "A"
+  value   = "45667e7e73834265b53e6a9e770a8554.cfargotunnel.com"
+  type    = "CNAME"
   proxied = var.zaffiliate_proxied
   ttl     = var.zaffiliate_proxied ? 1 : 300
 
-  comment = "Affiliate Automation OS - managed by Terraform"
+  comment = "Affiliate Automation OS via Cloudflare Tunnel - managed by Terraform"
 }
 
 output "zaffiliate_hostname" {
-  value = "${cloudflare_record.zaffiliate_a.name}.zeaz.dev"
+  value = "${cloudflare_record.zaffiliate_cname.name}.zeaz.dev"
 }
 
 output "zaffiliate_proxied" {
-  value = cloudflare_record.zaffiliate_a.proxied
+  value = cloudflare_record.zaffiliate_cname.proxied
 }
